141c07fd32c67bb2f5a2b401661ad8eedb325859
chenx15 <chenx15@rose-hulman.edu>
2018-09-02 16:28:31 -0400

    finished

--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,10 +2,10 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Xiaolong.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -38,3 +38,16 @@ Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ###############################################################################
+import rosegraphics as rg;
+window = rg.TurtleWindow();
+myturtle= rg.SimpleTurtle();
+myturtle.pen=rg.Pen('blue',1)
+myturtle.left(90);
+myturtle.forward(200);
+myturtle.pen_up();
+myturtle.go_to(100, -40);### a problem occured
+myturtle.pen_down();
+myturtle.pen=rg.Pen('green',10);
+myturtle.left(180);
+myturtle.forward(150);
+window.close_on_mouse_click();
